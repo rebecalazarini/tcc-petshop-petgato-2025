574974fc8ccb4ac54c9abf1393362c4820aa3cf0
rebecalazarini <rebecalazarii2007@gmail.com>
2025-02-26 15:11:29 -0300

    tcc

---
+++ JS/js1.js
@@ -0,0 +1,13 @@
+let EmailRef = document.getElementById("Email");
+let SenhaRef = document.getElementById("Senha");
+
+function Cadastrar() {
+var Email = document.getElementById('Email').value;
+var Senha = document.getElementById('Senha').value;
+if (Email,Senha.trim() === "") {
+window.alert("Campo vazio");
+document.getElementById('Email,Senha').value;
+} else {
+window.alert("Cadastrado com sucesso!");
+}
+}
---
+++ JS/js2.js
@@ -0,0 +1,13 @@
+let Email = document.getElementById("Email");
+let Password = document.getElementById("Password");
+
+function Login() {
+var Email = document.getElementById('Email').value;
+var Password = document.getElementById('Password').value;
+if (Email,Password.trim() === "") {
+window.alert("Campo vazio");
+document.getElementById('Email,Password').value;
+} else {
+window.alert("Cadastrado com sucesso!");
+}
+}
