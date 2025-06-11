48a1e9f4a960958bb41405f9c99104495b5b8f7f
rebecalazarini <rebecalazarini2007@gmail.com>
2025-06-11 16:43:52 -0300

    alterações

--- front/web/JS/js7.js
+++ front/web/JS/js7.js
@@ -5,3 +5,21 @@ menuToggle.addEventListener('click',() => {
    slideMenu.classList.toggle('active');
    menuToggle.classList.toggle('active');
 });
+
+
+
+
+function abrirModal() {
+  document.getElementById("meuModal").style.display = "block";
+}
+
+function fecharModal() {
+  document.getElementById("meuModal").style.display = "none";
+}
+
+window.onclick = function(event) {
+  const modal = document.getElementById("meuModal");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
